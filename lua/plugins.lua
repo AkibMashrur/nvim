256241f26963723f01aa59545b0efaa5fbad1d56
Akib Mashrur <akibmashrur@gmail.com>
2022-09-19 11:44:21 +1000

    added mason

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -52,6 +52,14 @@ return packer.startup(function(use)
 			run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
 	}
 
+	-- Mason for external tools (LSP, linter, formatter, etc.)
+use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+}
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
