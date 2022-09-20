f7d584d6f5ba33b1ff95ba0e495555f56c7df2e7
Akib Mashrur <akibmashrur@gmail.com>
2022-09-21 00:03:16 +1000

    testing pm

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,7 +64,6 @@ return packer.startup(function(use)
 	-- Tree Explorer
 	use "kyazdani42/nvim-tree.lua"
 
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- test.c
+++ test.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-	printf("Hello world!");
+	int list[5];
+	list[0] = 23;
+	list[1] = 343423;
+	for (int i = 0; i < 5; i++)
+	{
+		list[i] = list[i] * 2;
+		printf("Hello world! %d \n", list[i]);
+	}
 	return 0;
 }
